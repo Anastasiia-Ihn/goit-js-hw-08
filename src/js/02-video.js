2b856e01455810c25c804fcf71162df54c128682
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-08-10 08:43:45 +0200

    add task 1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const throttle = require('lodash.throttle');
+const USER_STOPED_VIDEO_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTimeUser = localStorage.getItem(USER_STOPED_VIDEO_KEY);
+const parsedCurrentTimeUser = JSON.parse(currentTimeUser) ?? []; 
+
+// videoplayer-current-time
+
+
+function onPlay (data) { 
+        
+    localStorage.setItem(USER_STOPED_VIDEO_KEY, JSON.stringify(data.seconds)); 
+
+ console.log(JSON.stringify(data.seconds));
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+// player.setCurrentTime(parsedCurrentTimeUser).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
