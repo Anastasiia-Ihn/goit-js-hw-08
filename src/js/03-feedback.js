c9b543a97d430b7c3e229a3200692e1a94ac2e16
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-08-11 23:09:24 +0200

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form')
+const KEY_FEEDBACK = "feedback-form-state"
+
+let feedbackData= {}; 
+
+const {elements: { email, message }} = feedbackForm;
+
+feedbackForm.addEventListener('input', throttle(handlerSaveText, 500))
+feedbackForm.addEventListener('submit', onClickSubmit)
+  
+ updateOutput();
+
+function handlerSaveText(evt) {
+
+  if (evt.currentTarget) {
+    feedbackData = { email: email.value, message: message.value }
+    localStorage.setItem(KEY_FEEDBACK, JSON.stringify(feedbackData));
+}
+}
+
+function onClickSubmit(evt) {
+  
+  evt.preventDefault();  // щоб неперезавантажувалося 
+  evt.currentTarget.reset();  // зкидує всі значення полів введення
+  localStorage.removeItem(KEY_FEEDBACK);  // зачищаємо localStorage
+}
+function updateOutput() {
+    
+const saveFeedback =JSON.parse(localStorage.getItem(KEY_FEEDBACK)) || '';
+  
+  email.value = saveFeedback.email;
+  message.value = saveFeedback.message;
+    
+}
